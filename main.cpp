cc88c195247de3c660c90aec5bb5ec8a3892de9b
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-07-06 15:18:48 +0300

    Add sprites for background and car. Load and display them.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 
 int main() {
     // Create a window
-    sf::RenderWindow window(sf::VideoMode(480, 480), "Racing");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Racing");
+    window.setFramerateLimit(60);
+
+    // Load sprites
+    sf::Sprite background, car;
+    sf::Texture t_back, t_car;
+    t_back.loadFromFile("assets/background.png");
+    t_car.loadFromFile("assets/car.png");
+    background.setTexture(t_back);
+    car.setTexture(t_car);
 
     // Main loop
     while (window.isOpen()) {
@@ -12,8 +21,15 @@ int main() {
                 window.close();
             }
         }
-
+        // Draw background
         window.clear(sf::Color::White);
+        window.draw(background);
+
+        // Draw a car
+        car.setPosition(300, 300);
+        car.setColor(sf::Color::Green);
+        window.draw(car);
+
         window.display();
     }
     return 0;
