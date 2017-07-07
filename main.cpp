afb21cf6b347c102f2e4c01ff2e6390f59a36da9
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-07-07 14:58:05 +0300

    Clean all of the obsolete instructions; Pass Map::calculateOffset function to Car::update to properly update their positions relatively to each other

--- main.cpp
+++ main.cpp
@@ -11,18 +11,8 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(640, 480), "Racing");
     window.setFramerateLimit(60);
 
-//    // TODO Map-related variables
-//    int background_w = background.getTextureRect().width;
-//    int background_h = background.getTextureRect().height;
-
+    // Setup actors
     Map map;
-
-    // TODO Car-related variables
-    float x = 300, y=300;
-
-    // Screen offset
-    struct { int x{ 0 }; int y{ 0 }; } offset;
-
     Car car(320, 240, sf::Color::Green);
 
     // Main loop
@@ -33,24 +23,18 @@ int main() {
                 window.close();
             }
         }
-        // Handle keyboard input loop
+        // Handle keyboard input
         Input::handle({ sf::Keyboard::Up }, [&car](){car.move(Direction::Up);});
         Input::handle({ sf::Keyboard::Down }, [&car](){car.move(Direction::Down);});
         Input::handle({ sf::Keyboard::Up, sf::Keyboard::Down }, [&car](){car.move(Direction::None);});
         Input::handle({ sf::Keyboard::Left }, [&car](){car.move(Direction::Left);});
         Input::handle({ sf::Keyboard::Right },[&car](){car.move(Direction::Right);});
 
-//        // Update car's position and rotation as well as background offset to display map correctly
-//        x += sin(angle) * speed;
-//        y -= cos(angle) * speed;
-//        x > 320 && background_w - x > 320 ? (offset.x = x - 320) : 0;
-//        y > 240 && background_h - y > 240 ? (offset.y = y - 240) : 0;
-//        background.setPosition(-offset.x, -offset.y);
+        // Update
+        car.update([&map](sf::Vector2f position){return map.calculateOffset(position);});
+        map.update();
 
-        // Update loop
-        car.update();
-
-        // Draw loop
+        // Draw
         window.clear(sf::Color::White);
         map.draw(window);
         car.draw(window);
